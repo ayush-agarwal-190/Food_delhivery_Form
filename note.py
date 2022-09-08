9bc3da2233e497137d1034854bbe3eef62a4efdd
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-09-08 14:10:32 +0530

    Add files via upload

---
+++ note.py
@@ -0,0 +1,54 @@
+import mysql.connector
+from mysql.connector import Error
+
+from tkinter import *
+
+root = Tk()
+
+def getvals():
+    print("It works!")
+root.geometry("644x344")
+#Heading
+Label(root, text="Welcome to Ayush Travelling Agency", font="comicsansms 13 bold", pady=15).grid(row=0, column=50)
+
+#Text for our form
+name = Label(root, text="Name")
+phone = Label(root, text="Phone")
+gender = Label(root, text="Gender")
+emergency = Label(root, text="Contact Name")
+paymentmode = Label(root, text="Payment Method")
+
+#Pack text for our form
+name.grid(row=1, column=2)
+phone.grid(row=2, column=2)
+gender.grid(row=3, column=2)
+emergency.grid(row=4, column=2)
+paymentmode.grid(row=5, column=2)
+
+# Tkinter variable for storing entries
+namevalue = StringVar()
+phonevalue = StringVar()
+gendervalue = StringVar()
+emergencyvalue = StringVar()
+paymentmodevalue = StringVar()
+foodservicevalue = IntVar()
+
+
+#Entries for our form
+nameentry = Entry(root, textvariable=namevalue)
+phoneentry = Entry(root, textvariable=phonevalue)
+genderentry = Entry(root, textvariable=gendervalue)
+emergencyentry = Entry(root, textvariable=emergencyvalue)
+paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
+
+# Packing the Entries
+nameentry.grid(row=1, column=3)
+phoneentry.grid(row=2, column=3)
+genderentry.grid(row=3, column=3)
+emergencyentry.grid(row=4, column=3)
+paymentmodeentry.grid(row=5, column=3)
+
+Button(text="Submit to Harry Travels", command=getvals).grid(row=7, column=3)
+
+
+root.mainloop()
